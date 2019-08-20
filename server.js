38f659c189bffe693ed201e78b12d919826daeed
Andres Arango <andres.ao568@gmail.com>
2019-08-20 14:10:56 -0500

    fixed server route

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
+//Install express server
 const express = require('express');
 const path = require('path');
+
 const app = express();
 
-// Serve static files....
-app.use(express.static(__dirname + '/dist/mercado-libre-search'));
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/mercadoLibre-search'));
 
-// Send all requests to index.html
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/mercado-libre-search/index.html'));
+app.get('/*', function (req, res) {
+
+  res.sendFile(path.join(__dirname + '/dist/mercadoLibre-search/index.html'));
 });
 
-// default Heroku PORT
-app.listen(process.env.PORT || 3000);
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
